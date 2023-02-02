9fd09662a47be5853a6d64af3360afa64c7913f1
Matsiuk Aliaksei <murryaa@gmai.com>
2023-02-02 20:55:24 +0300

    Add 4

--- work_dir/fun.py
+++ work_dir/fun.py
@@ -1,8 +1,11 @@
+# Импортируем словарь
 from jeson_file import result
-#from datetime import datetime
-#from pprint import pprint
 
 def if_EXECUTED():
+    """
+    Сортируем все транзакции по Выполненым или Невыполненым
+    :return: Возврощает отсортированый список
+    """
     exc = []
     for i in range(len(result)):
         if "state" not in result[i]:
@@ -12,30 +15,44 @@ def if_EXECUTED():
         if result[i]["state"] == "EXECUTED":
             exc.append(result[i])
     return exc
-#pprint(if_EXECUTED())
+
 
 def last_five():
+    """
+    Из сортировоного списа берем пять последних по дате
+    :return: Возврощает список из 5 транзакций
+    """
     new = if_EXECUTED()[::-1]
     lis_new = sorted(new[:5], key=lambda data: data["date"])
     return lis_new[::-1]
 
-#pprint(last_five())
 
 def data_time(data):
-
+    """
+    Делаем дату из словаря
+    :param data: строчка из словаря с датой
+    :return:дата (день, месяц, год)
+    """
     data = data.split("-")
     index_data_2 = [data[2][:2], data[1], data[0]]
-        #the_data = datetime.strptime("-".join(index_data_2), "%d-%m-%Y")
     return ".".join(index_data_2)
 
-#print(data_time("2019-07-12T20:41:47.882230"))
 
 def organization_transfer(description):
+    """
+    Организация транзакции
+    :param description: строка из словаря
+    :return: транзакция
+    """
     return description
 
-#organization_transfer()
 
 def number_cart_send(send):
+    """
+    Выводим Номер карты и систему оплаты
+    :param send: номер счёта отправителя
+    :return: возврощает скрытый счёт
+    """
     account_number = send.split(' ')[-1]
     if len(account_number) == 20:
         return f'Счет **{account_number[16:]}'
@@ -44,9 +61,13 @@ def number_cart_send(send):
                f'{account_number[0:4]} {account_number[4:6]}' \
                f'** **** {account_number[12:]}'
 
-#print(number_cart_send("Visa Classic 6831982476737658"))
 
 def number_cart_recipient(recipient):
+    """
+    Выводим Номер карты и систему оплаты
+    :param recipient: номер счёта получателя
+    :return: возврощает скрытый счёт
+    """
     account_number = recipient.split(' ')[-1]
     if len(account_number) == 20:
         return f'Счет **{account_number[16:]}'
@@ -55,9 +76,12 @@ def number_cart_recipient(recipient):
                f'{account_number[0:4]} {account_number[4:6]}' \
                f'** **** {account_number[12:]}'
 
-#score()
 
 def money(amount_of_money, currency):
+    """
+    :param amount_of_money: Вводится колличество денег из словаря
+    :param currency: Вводится валюта из словаря
+    :return: Возврощает количество денег и волюту
+    """
     return f'{amount_of_money} {currency}'
 
-#print(money("97853.86", "руб."))
--- work_dir/jeson_file.py
+++ work_dir/jeson_file.py
@@ -1,11 +1,6 @@
+# Импортируется json file из внешнего источника
 import requests as requests
-
 
 URL = "https://www.jsonkeeper.com/b/0MZI"
 
 result = requests.get(URL).json()
-
-# for i in range(len(result)):
-#     if "state" not in result[i]:
-#         continue
-#     print(result[i]["state"])
